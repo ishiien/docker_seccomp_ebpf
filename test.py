758a477b1e8d075f4c8e5443303257dbbc0b8e68
ishii211 <yuya211@icloud.com>
2022-12-14 16:32:53 +0900

    some container id get from Docker API

---
+++ test.py
@@ -0,0 +1,36 @@
+from docker_sdk import dockerfile,docker_sdk
+from docker_start import run_trace
+from time import sleep
+import subprocess
+
+# container_list is list of containers to be traced
+container_name_list = []
+command_list = []
+dockerfile.Check_Docker_Compose(container_name_list)
+dockerfile.Check_Docker_Compose_CMD(command_list)
+
+if not container_name_list:
+    print("container id is not set")
+    exit(1)
+
+#Create Docker container
+print("container create now")
+dockerfile.Create_Container_Test()
+
+# asynchronous processing
+container_id_list = []
+list_length = len(container_name_list)
+count_length = 0
+container_id = 0
+while list_length > count_length:
+    for container_name in container_name_list:
+        container_id = docker_sdk.ContainerName_to_ContainerId(container_name)
+        if container_id != 0 and container_id not in container_id_list:
+            container_id_list.append(container_id)
+            count_length = count_length + 1
+
+print(container_id_list)
+
+
+
+
